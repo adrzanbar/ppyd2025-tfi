7663edbf081f5e1de574f41b30f37d3feeb068d4
Rysen-Winters <lcastinelli@gmail.com>
2025-06-02 20:21:02 -0300

    Added sequential-experiments.cpp for running experiments with sequential algorithm

---
+++ sequential-experiments.cpp
@@ -0,0 +1,150 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <random>
+#include <string>
+#include <iomanip>
+#include <chrono>
+#include <fstream>
+
+// Funciones para darle formato a los numeros para imprimirlos por pantalla.
+std::string format_double(double number)
+{
+    std::ostringstream oss;
+    oss << std::fixed << std::showpoint << std::setprecision(1);
+    oss << number;
+    return oss.str();
+}
+
+std::string center_num(double number, int total_ln)
+{
+    std::string num_str = format_double(number);
+    int espacios = total_ln - num_str.length();
+    int derecha = espacios / 2;
+    int izquierda = espacios - derecha;
+    return std::string(izquierda, ' ') + num_str + std::string(derecha, ' ');
+}
+// ---
+
+struct Matrix
+{
+    int rows;
+    int cols;
+    std::vector<double> data;
+
+    Matrix(int r, int c) : rows(r), cols(c), data(r * c) {}
+
+    double *operator[](int row)
+    {
+        return &data[row * cols];
+    }
+
+    const double *operator[](int row) const
+    {
+        return &data[row * cols];
+    }
+
+    Matrix operator*(const Matrix &other) const
+    {
+        if (cols != other.rows)
+        {
+            throw std::invalid_argument("Matrix dimensions do not match for multiplication.");
+        }
+        Matrix result(rows, other.cols);
+
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < other.cols; ++j)
+            {
+                double sum = 0.0;
+                for (int k = 0; k < cols; ++k)
+                {
+                    sum += (*this)[i][k] * other[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+        return result;
+    }
+
+    void rand()
+    {
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::uniform_real_distribution<double> dis(0.0, 1.0);
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                (*this)[i][j] = dis(g);
+            }
+        }
+    }
+
+    int calculate_biggest_ln() const
+    { // Convierte a string los valores de la matriz para medir la longitud de la cadena, devuelve el más grande.
+        int max_len = 0;
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                std::string val_str = format_double((*this)[i][j]);
+                int len = val_str.length();
+                if (len > max_len)
+                {
+                    max_len = len;
+                }
+            }
+        }
+        return max_len;
+    }
+
+    std::string str() const
+    {
+        int biggest_ln = calculate_biggest_ln();
+
+        std::ostringstream oss;
+        for (int i = 0; i < rows; ++i)
+        {
+            // oss << "|";
+            for (int j = 0; j < cols; ++j)
+            {
+                oss << center_num((*this)[i][j], biggest_ln) << " ";
+            }
+            oss << "\n";
+        }
+        return oss.str();
+    }
+};
+
+int main()
+{
+    std::string filename = "resultados_sequential.csv";
+    std::ofstream file(filename, std::ios::app);
+
+    std::vector<int> dimensions = {1000, 2000, 3000, 4000, 8000};
+    int iterations = 3;
+    file << "dimension,tiempo" << std::endl;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    std::chrono::duration<double> total_time(0);
+
+    for (auto& dimension: dimensions){
+      for (int i = 0; i < iterations; i++)
+      {
+        Matrix A(dimension, dimension);
+        Matrix B(dimension, dimension);
+        A.rand();
+        B.rand();
+        std::cout << "Ejecutando experimento para dimension: " << dimension << std::endl;
+        start = std::chrono::high_resolution_clock::now();
+        A* B;
+        end = std::chrono::high_resolution_clock::now();
+        total_time += end - start;
+      }
+      std::cout << "Tiempo promedio: " << total_time.count() / iterations << std::endl;
+      file << dimension << "," << total_time.count() / iterations << std::endl;
+      total_time = std::chrono::duration<double>(0);
+    }
+
+    return 0;
+}
